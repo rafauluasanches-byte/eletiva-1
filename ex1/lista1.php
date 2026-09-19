20d5a1d0ff152b8210183130bdf3ef41f23634d9
Rafaela Sanches <rafauluasanches@gmail.com>
2026-09-19 16:13:42 -0300

    exercicios 9 a 19 - lista 1

--- ex1/lista1.php
+++ ex1/lista1.php
@@ -8,7 +8,7 @@
 </head>
 <body> 
     <div class="container py-3">
-        <h1>Exercícios - Lista 1</h1>
+        <h1>Exercícios - Lista 1 (5 a 19)</h1>
         <form method="post" action="repostalista1.php">
             <h3>Exercicio 5</h3>
             <div class="mb-3">
@@ -33,6 +33,95 @@
             <div class="mb-3">
                 <label for="raio" class="form-label">Digite o raio do circulo</label>
                 <input type="number" id="raio" name="raio" class="form-control" required="">
+            </div>
+            <h3>Exercício 9</h3>
+            <div class="mb-3">
+              <label for="altR" class="form-label">Digite a altura do retangulo</label>
+              <input type="number" id="altR" name="altR" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="largR" class="form-label">Digite a largura do retangulo</label>
+              <input type="number" id="largR" name="largR" class="form-control" required="">
+            </div>
+            <h3>Exercício 10</h3>
+            <div class="mb-3">
+              <label for="r1" class="form-label">Digite o raio do circulo</label>
+              <input type="number" id="r1" name="r1" class="form-control" required="">
+            </div>
+            <h3>Exercício 11</h3>
+            <div class="mb-3">
+              <label for="base" class="form-label">Informe a base da potencia</label>
+              <input type="number" id="base" name="base" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="expoente" class="form-label">Informe o expoente</label>
+              <input type="number" id="expoente" name="expoente" class="form-control" required="">
+            </div>
+            <h3>Exercício 12</h3>
+            <div class="mb-3">
+              <label for="metro" class="form-label">Informe a quantidade em metros</label>
+              <input type="number" id="metro" name="metro" class="form-control" required="">
+            </div>
+            <h3>Exercício 13</h3>
+            <div class="mb-3">
+              <label for="km" class="form-label">Informe a quantidade em Km</label>
+              <input type="number" id="km" name="km" class="form-control" required="">
+            </div>
+            <h3>Exercício 14</h3>
+            <div class="mb-3">
+              <label for="peso" class="form-label">Informe o seu peso</label>
+              <input type="number" id="peso" name="peso" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="alt" class="form-label">Informe a sua altura</label>
+              <input type="number" id="alt" name="alt" class="form-control" required="">
+            </div>
+            <h3>Exercício 15</h3>
+            <div class="mb-3">
+              <label for="preco" class="form-label">Digite o preço</label>
+              <input type="number" id="preco" name="preco" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="desconto" class="form-label">Digite a porcentagem de desconto</label>
+              <input type="number" id="desconto" name="desconto" class="form-control" required="">
+            </div>
+            <h3>Exercício 16</h3>
+            <div class="mb-3">
+              <label for="capital1" class="form-label">Digite o valor do capital</label>
+              <input type="number" id="capital1" name="capital1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="taxa1" class="form-label">Digite o valor da taxa</label>
+              <input type="number" id="taxa1" name="taxa1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="periodo1" class="form-label">Digite o periodo</label>
+              <input type="number" id="periodo1" name="periodo1" class="form-control" required="">
+            </div>
+            <h3>Exercício 17</h3>
+            <div class="mb-3">
+              <label for="dias" class="form-label">Informe o valor em dias</label>
+              <input type="number" id="dias" name="dias" class="form-control" required="">
+            </div>
+            <h3>Exercício 18</h3>
+            <div class="mb-3">
+              <label for="capital2" class="form-label">Digite o valor do capital do juros compostos</label>
+              <input type="number" id="capital2" name="capital2" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="taxa2" class="form-label">Digite o valor da taxa</label>
+              <input type="number" id="taxa2" name="taxa2" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="periodo2" class="form-label">Digite o período</label>
+              <input type="number" id="periodo2" name="periodo2" class="form-control" required="">
+            </div>
+            <h3>Exercício 19</h3>
+            <div class="mb-3">
+              <label for="dist" class="form-label">Digite a distancia</label>
+              <input type="number" id="dist" name="dist" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="tempo" class="form-label">Digite o tempo</label>
+              <input type="number" id="tempo" name="tempo" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
--- ex1/repostalista1.php
+++ ex1/repostalista1.php
@@ -23,5 +23,76 @@
         $areaC = $pi * $raio ** 2;
         echo "<p>Area do circulo $areaC</p>";
 
+		echo "<p>Exercício 9</p>";
+        $altR = $_POST['altR'];
+		$largR = $_POST['largR'];
+		$perimetroRetangulo = ($altR * 2) + ($largR * 2);
+		echo "<p>O perimetro do retangulo é: $perimetroRetangulo</p>";
+		
+		echo "<p>Exercício 10</p>";
+		$r1 = $_POST['r1'];
+		$pi = 3.1415;
+		$perimetroCirculo = 2 * $pi * $r1;
+		echo "<p>O perimetro do circulo é: $perimetroCirculo</p>";
+		
+		echo "<p>Exercício 11</p>";
+		$base = $_POST['base'];
+		$expoente = $_POST['expoente'];
+		$potencia = $base ** $expoente;
+		echo "<p>$potencia</p>";
+		
+		echo "<p>Exercício 12</p>";
+		$metro = $_POST['metro'];
+		$centimetro = $metro * 100;
+		echo "<p>$centimetro cm</p>";
+		
+		echo "<p>Exercício 13</p>";
+		$km = $_POST['km'];
+		$milha = $km / 1.609;
+		echo "<p>milha: $milha</p>";
+		
+		echo "<p>Exercício 14</p>";
+		$peso = $_POST['peso'];
+		$alt = $_POST['alt'];
+		$imc = $peso / $altura ** 2;
+		echo "<p>I.M.C: $imc</p>";
+		
+		echo "<p>Exercício 15</p>";
+		$preco = $_POST['preco'];
+		$desconto = $_POST['desconto'];
+		$precoDesc = $preco - ($preco * ($desconto / 100) );
+		echo "<p>preco com desconto: $precoDesc</p>";
+		
+		echo "<p>Exercício 16</p>";
+		$capital1 = $_POST['capital1'];
+		$taxa1 = $_POST['taxa1'];
+		$periodo1 = $_POST['periodo1'];
+		$jurosS = $capital1 * $taxa1 * $periodo1;
+		echo "<p>Juros Simples: $jurosS</p>";
+		
+		echo "<p>Exercício 17</p>";
+		$capital2 = $_POST['capital2'];
+		$taxa2 = $_POST['taxa2'];
+		$periodo2 = $_POST['periodo2'];
+		$jurosC = $capital2 * (1 + $taxa2) ** $periodo2;
+		echo "<p>Juros Compostos: $jurosC</p>";
+		
+		echo "<p>Exercício 18</p>";
+		$dias = $_POST['dias'];
+		$hora = $dias * 24;
+		$min = $hora * 60;
+		$seg = $min * 60;
+		echo "<p>Hora: $hora - Minuto: $min - Segundo: $seg</p>";
+		
+		echo "<p>Exercício 19</p>";
+		$dist = $_POST['dist'];
+		$tempo = $_POST['tempo'];
+		if($tempo != 0)
+            {
+                $vm = $dist / $tempo;
+			    echo "<p>$vm</p>";
+            }
+        else
+			echo "<p>Divisão por zero</p>";
     }
 ?>
